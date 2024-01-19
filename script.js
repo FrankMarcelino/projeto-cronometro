3dfdbe8793c6948a6d73bc0e8f3c53d946db0993
frankMarcelino <frank.silva.ti@gmail.com>
2024-01-19 13:23:02 -0300

    terminei o projeto

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const minutesEl = document.querySelector('#minutes')
 const secondsEl = document.querySelector('#seconds')
 const millisecondsEl = document.querySelector('#milliseconds')
 
-const startBtn= document.querySelector('#startBtn')
-const pauseBtn= document.querySelector('#pauseBtn')
+const startBtn = document.querySelector('#startBtn')
+const pauseBtn = document.querySelector('#pauseBtn')
 const resumeBtn = document.querySelector('#resumeBtn')
 const resetBtn= document.querySelector('#resetBtn')
 
@@ -15,6 +15,9 @@ let milliseconds = 0
 let isPaused = false
 
 startBtn.addEventListener('click', startTimer)
+pauseBtn.addEventListener('click', pauseTimer)
+resumeBtn.addEventListener('click', resumeTimer)
+resetBtn.addEventListener('click', resetTimer)
 
 function startTimer() {
     interval = setInterval(() => {
@@ -36,6 +39,39 @@ function startTimer() {
             millisecondsEl.textContent = formatMilliseconds(milliseconds)
         }
     },10)
+
+    startBtn.style.display = 'none'
+    pauseBtn.style.display = 'block'
+    resetBtn.style.display = 'block'
+}
+
+function pauseTimer() {
+    isPaused = true
+    pauseBtn.style.display = 'none'
+    resumeBtn.style.display = 'block'
+}
+
+function resumeTimer() {
+    isPaused = false
+    pauseBtn.style.display = 'block'
+    resumeBtn.style.display = 'none'
+
+}
+
+function resetTimer() {
+    clearInterval(interval)
+    minutes = 0
+    seconds = 0
+    milliseconds = 0
+
+    minutesEl.textContent = '00'
+    secondsEl.textContent = '00'
+    millisecondsEl.textContent = '000'
+
+    startBtn.style.display = 'block'
+    pauseBtn.style.display = 'none'
+    resumeBtn.style.display = 'none'
+    resetBtn.style.display = 'none'
 }
 
 function formatTime(time) {
